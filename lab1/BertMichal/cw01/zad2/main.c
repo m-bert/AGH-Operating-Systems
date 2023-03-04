06be1c4f2ec8e5b2dd8eb238afb8d8ad54f20c72
Michal Bert <misiek.b3870@gmail.com>
2023-03-04 17:24:34 +0100

    Update task 2

--- lab1/BertMichal/cw01/zad2/main.c
+++ lab1/BertMichal/cw01/zad2/main.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include "../zad1/lib.h"
+
+void perform_action(WordCounter *word_counter, char *command, char *arg1, char *arg2);
 
 int main()
 {
+    WordCounter *word_counter;
+
     const char delimiter[] = " ";
 
     char line_buffer[1024];
 
     while (fgets(line_buffer, sizeof(line_buffer), stdin) != NULL)
     {
-        char *word = strtok(line_buffer, delimiter);
-        printf("%s", word);
+        line_buffer[strcspn(line_buffer, "\n")] = 0; // Get rid of \n
 
-        word = strtok(NULL, delimiter);
-        printf("%s", word);
+        char *command = strtok(line_buffer, delimiter);
+        char *arg1 = strtok(NULL, delimiter);
+        char *arg2 = strtok(NULL, delimiter);
+
+        perform_action(word_counter, command, arg1, arg2);
     }
 
     return 0;
+}
+
+void perform_action(WordCounter *word_counter, char *command, char *arg1, char *arg2)
+{
+    if (strcmp(command, "init") == 0)
+    {
+        printf("Initializing...\n");
+        return;
+    }
+
+    if (strcmp(command, "count") == 0)
+    {
+        printf("Counting words...\n");
+        return;
+    }
+
+    if (strcmp(command, "show") == 0)
+    {
+        printf("Show...\n");
+        return;
+    }
+
+    if (strcmp(command, "delete") == 0)
+    {
+        printf("Deleting...\n");
+        return;
+    }
+
+    if (strcmp(command, "destroy") == 0)
+    {
+        printf("Destroying...\n");
+        return;
+    }
 }
