331718aba41c4f3e9cbc0cc85039203d9c282af1
Michal Bert <misiek.b3870@gmail.com>
2023-03-04 22:25:14 +0100

    Remove new line in

--- lab1/BertMichal/cw01/zad1/lib.c
+++ lab1/BertMichal/cw01/zad1/lib.c
@@ -57,7 +57,7 @@ void perform_counting(WordCounter *word_counter, char *filename)
     if (buffer)
     {
         word_counter->elements[word_counter->size] = calloc(length - 1, sizeof(char));
-        memmove(word_counter->elements[word_counter->size], buffer, length * sizeof(char));
+        memmove(word_counter->elements[word_counter->size], buffer, (length - 1) * sizeof(char));
 
         word_counter->size++;
 
