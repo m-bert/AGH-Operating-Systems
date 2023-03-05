295bae4b8074b5d28e230cc43526f83c86013936
Michal Bert <misiek.b3870@gmail.com>
2023-03-05 16:23:38 +0100

    Add timers

--- lab1/BertMichal/cw01/zad2/main.c
+++ lab1/BertMichal/cw01/zad2/main.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/times.h>
+#include <inttypes.h>
 
 #include "lib.h"
 
-void perform_action(WordCounter *word_counter, char *command, char *arg1, char *arg2);
+void print_time(struct tms time_start, struct tms time_end, clock_t real_start, clock_t real_end);
 
-int main()
+int main(int argc, char **argv)
 {
     WordCounter *word_counter = NULL;
 
-    const char delimiter[] = " ";
+    const char uninitialized_error_message[] = "Error: word_counter not initialized!\n";
 
+    const char delimiter[] = " ";
     char line_buffer[1024];
+    char *command, *arg1, *arg2;
+
+    struct tms start_time, end_time;
+    clock_t real_start, real_end;
 
     while (fgets(line_buffer, sizeof(line_buffer), stdin) != NULL)
     {
-        line_buffer[strcspn(line_buffer, "\n")] = 0; // Get rid of \n
+        // Get rid of \n
+        line_buffer[strcspn(line_buffer, "\n")] = 0;
 
-        char *command = strtok(line_buffer, delimiter);
-        char *arg1 = strtok(NULL, delimiter);
-        char *arg2 = strtok(NULL, delimiter);
+        // Get args from line
+        command = strtok(line_buffer, delimiter);
+        arg1 = strtok(NULL, delimiter);
+        arg2 = strtok(NULL, delimiter);
 
+        // Perform actions
         if (strcmp(command, "init") == 0)
         {
             if (word_counter != NULL)
@@ -30,28 +42,47 @@ int main()
                 continue;
             }
 
-            word_counter = init_counter(atoi(arg1));
-        }
-
-        if (strcmp(command, "count") == 0)
-        {
-            if (word_counter == NULL)
+            if (arg1 == NULL)
             {
-                printf("Error: word_counter not initialized!\n");
+                printf("You have to provide size for the word counter!\n");
                 continue;
             }
 
+            int size = atoi(arg1);
+
+            if (size <= 0)
+            {
+                printf("You have to provide a positive size!\n");
+                continue;
+            }
+
+            real_start = times(&start_time);
+            word_counter = init_counter(size);
+            real_end = times(&end_time);
+        }
+
+        else if (strcmp(command, "count") == 0)
+        {
+            if (word_counter == NULL)
+            {
+                printf(uninitialized_error_message);
+                continue;
+            }
+
+            real_start = times(&start_time);
             perform_counting(word_counter, arg1);
+            real_end = times(&end_time);
         }
 
-        if (strcmp(command, "show") == 0)
+        else if (strcmp(command, "show") == 0)
         {
             if (word_counter == NULL)
             {
-                printf("Error: word_counter not initialized!\n");
+                printf(uninitialized_error_message);
                 continue;
             }
 
+            real_start = times(&start_time);
             char *element = get_element_at(word_counter, atoi(arg1));
 
             if (element == NULL)
@@ -61,31 +92,55 @@ int main()
             }
 
             printf("%s\n", element);
+            real_end = times(&end_time);
         }
 
-        if (strcmp(command, "delete") == 0 && strcmp(arg1, "index") == 0)
+        else if (strcmp(command, "delete") == 0 && strcmp(arg1, "index") == 0)
         {
             if (word_counter == NULL)
             {
-                printf("Error: word_counter not initialized!\n");
+                printf(uninitialized_error_message);
                 continue;
             }
 
+            real_start = times(&start_time);
             remove_element_at(word_counter, atoi(arg2));
+            real_end = times(&end_time);
         }
 
-        if (strcmp(command, "destroy") == 0)
+        else if (strcmp(command, "destroy") == 0)
         {
             if (word_counter == NULL)
             {
-                printf("Error: word_counter not initialized!\n");
+                printf(uninitialized_error_message);
                 continue;
             }
 
+            real_start = times(&start_time);
             destroy(word_counter);
             word_counter = NULL;
+            real_end = times(&end_time);
         }
+
+        else
+        {
+            printf("Unknown command\n");
+            continue;
+        }
+
+        print_time(start_time, end_time, real_start, real_end);
     }
 
     return 0;
+}
+
+void print_time(struct tms start_time, struct tms end_time, clock_t real_start, clock_t real_end)
+{
+    clock_t real_time = real_end - real_start;
+    clock_t user_time = end_time.tms_utime - start_time.tms_utime;
+    clock_t system_time = end_time.tms_stime - start_time.tms_stime;
+
+    printf("real_time: %.15lf\n", (double)(real_time * 1000 / sysconf(_SC_CLK_TCK)));
+    printf("user_time: %.15lf\n", (double)(user_time * 1000 / sysconf(_SC_CLK_TCK)));
+    printf("system_time: %.15lf\n", (double)(system_time * 1000 / sysconf(_SC_CLK_TCK)));
 }
