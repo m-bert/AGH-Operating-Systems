76ce2b137b22517fcf5ce8cb2ee65505cae682cb
Michal Bert <misiek.b3870@gmail.com>
2023-03-12 16:07:27 +0100

    Add comments and fix edge case

--- lab2/BertMichal/cw02/zad2/reverse.c
+++ lab2/BertMichal/cw02/zad2/reverse.c
@@ -10,11 +10,13 @@ bool reverse_file(const char *input_path, const int block_size, FILE *input_file
 {
     char *buffer = malloc(sizeof(char) * block_size);
 
-    // Go to end of file
+    // Go to last block of file
     int current_position = 0;
     fseek(input_file, -block_size * sizeof(char), SEEK_END);
 
     // This loops runs while we can read whole block of data
+    // current_position == ftell(input_file) means that going back by block_size would result in going to negative position
+    // When this happens, we either read whole file, or there are still bytes left to read, however their size is smaller than block_size
     while (ftell(input_file) != current_position)
     {
         current_position = ftell(input_file);
@@ -23,27 +25,51 @@ bool reverse_file(const char *input_path, const int block_size, FILE *input_file
         reverse_str(buffer);
         fwrite(buffer, sizeof(char), block_size * sizeof(char), output_file);
 
-        fseek(input_file, -block_size * sizeof(char), SEEK_CUR); // Returns to current_position
+        // Instead of going x2 block_size, we go back by block_size to reach last position
+        fseek(input_file, -block_size * sizeof(char), SEEK_CUR); // Going back so that ftell(input_file) == current_position
+
+        // Now we try to go back by block_size again
+        // If after that operation ftell(input_file) == current_position, then it means that there are still bytes left to read,
+        // however their size is smaller than block_size, so we have to read them manually
         fseek(input_file, -block_size * sizeof(char), SEEK_CUR);
     }
 
-    // Since while loop reads only full blocks, we have to read remaing part of the file
+    // Since while loop reads only full blocks, we have to read remaing part of the file,
     // which length doesn't have to be a multiple of block_size
     int remaining_bytes = ftell(input_file);
     fseek(input_file, 0, SEEK_SET);
 
+    // If block size == 1, it is not possible to be left with bytes that are not multiple of block_size
+    // In that case, we simply free the buffer and return from function
     if (block_size == 1)
     {
         free(buffer);
         return true;
     }
 
+    // If remaining_bytes == 0, then we either read whole file in while loop and ended on the beginning of the file,
+    // The other possibility is that input file is smaller than block_size
+    // To check that, we read block from file and see how much bytes have been read
     if (remaining_bytes == 0)
     {
+        // Read block from file and store value in remaining_bytes
         remaining_bytes = fread(buffer, sizeof(char), block_size * sizeof(char), input_file);
+
+        // If remaining_bytes, i.e. number of bytes we have just read is equal to block_size,
+        // that means that we have read this block already in the while loop, so we can return from function
+        if (remaining_bytes == block_size)
+        {
+            free(buffer);
+
+            return true;
+        }
+
+        // Read remaining bytes from file
         reverse_str(buffer);
         fwrite(buffer, sizeof(char), remaining_bytes * sizeof(char), output_file);
     }
+    // This branch represents situation, when current position in file is less than block_size.
+    // In that case, we have to manually read remaining bytes from file
     else
     {
         char *tmp_buffer = malloc(sizeof(char) * remaining_bytes);
