e66d64ccaf4e6cf03733fa73c48281d45bda9ee1
Michal Bert <misiek.b3870@gmail.com>
2023-03-16 14:11:46 +0100

    Task 2

--- lab3/BertMichal/cw03/zad2/main.c
+++ lab3/BertMichal/cw03/zad2/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        printf("Too few arguments\n");
+        return -1;
+    }
+
+    const char *path = argv[1];
+
+    printf("%s ", argv[0]);
+
+    fflush(stdout);
+
+    execl("/bin/ls", "ls", path, NULL);
+
     return 0;
 }
