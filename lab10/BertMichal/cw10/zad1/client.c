7f36080f3b8be4e307027ba5ed2ea0b118e46ef8
Michał Bert <63123542+m-bert@users.noreply.github.com>
2023-05-30 18:23:44 +0200

    Lab10 - Worst nightmare kill me pls (#11)
    * I. HATE. THIS. TASK.
    
    * Start of pings... I guess
    
    * Well, at least segv is gone... hopefully
    
    * Pings work (At least I think so)
    
    * Add logs
    
    * I think it works
    
    * I wannna die
    
    * Add resue
    
    * I. HATE. THIS. TASK.
    
    * Add info after SIGINT and remove these fucking flags from Makefile

---
+++ lab10/BertMichal/cw10/zad1/client.c
@@ -0,0 +1,239 @@
+#include "common.h"
+
+typedef enum Connection_Type
+{
+    WEB,
+    LOCAL
+} Connection_Type;
+
+char nick[MAX_NICK];
+Connection_Type connection_type;
+char *server_ip;
+char *UNIX_PATH;
+
+Event_Data *event_data;
+pthread_t read_thread;
+
+int PORT,
+    SOCKET_FD, EPOLL_FD;
+
+bool running = true;
+
+void init_socket();
+
+void handle_stop();
+void force_stop();
+void *read_messages();
+
+int main(int argc, char *argv[])
+{
+    UNIX_PATH = calloc(MAX_PATH, sizeof(char));
+
+    if (argc < 4)
+    {
+        printf("Too few arguments!\n");
+        return -1;
+    }
+
+    if (strlen(argv[1]) >= MAX_NICK)
+    {
+        printf("Too long nickname!\n");
+        return -1;
+    }
+
+    strcpy(nick, argv[1]);
+
+    if (strcmp(argv[2], "WEB") == 0)
+    {
+        if (argc < 5)
+        {
+            printf("Provide port!\n");
+            return -1;
+        }
+
+        connection_type = WEB;
+        server_ip = argv[3];
+        PORT = atoi(argv[4]);
+    }
+    else if (strcmp(argv[2], "LOCAL") == 0)
+    {
+        connection_type = LOCAL;
+        strcpy(UNIX_PATH, argv[3]);
+    }
+    else
+    {
+        printf("Invalid connection type!\n");
+        return -1;
+    }
+
+    init_socket();
+
+    char *response = calloc(MAX_MSG, sizeof(char));
+    send(SOCKET_FD, nick, strlen(nick), 0);
+
+    recv(SOCKET_FD, (char *)response, MAX_MSG, 0);
+
+    if (strcmp(response, FULL) == 0)
+    {
+        free(response);
+        printf("Server full!\n");
+        return 0;
+    }
+
+    free(response);
+
+    pthread_create(&read_thread, NULL, read_messages, NULL);
+
+    struct sigaction action;
+    action.sa_handler = &handle_stop;
+    sigaction(SIGINT, &action, NULL);
+
+    char line_buffer[LINE_BUFFER_LENGTH] = "";
+    while (true)
+    {
+        fgets(line_buffer, sizeof(line_buffer), stdin);
+
+        if (strcmp(line_buffer, "\n") == 0)
+        {
+            continue;
+        }
+
+        send(SOCKET_FD, (char *)line_buffer, strlen(line_buffer), 0);
+
+        char *tmp = strtok(line_buffer, DELIMITER);
+
+        if (strcmp(tmp, STOP) == 0)
+        {
+            running = false;
+            break;
+        }
+    }
+
+    pthread_join(read_thread, NULL);
+    return 0;
+}
+
+void init_socket()
+{
+    if (connection_type == WEB)
+    {
+        SOCKET_FD = socket(AF_INET, SOCK_STREAM, 0);
+        if (SOCKET_FD == -1)
+        {
+            printf("Failed to open socket\n");
+            exit(0);
+        }
+
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        inet_pton(AF_INET, server_ip, &addr.sin_addr);
+
+        if (connect(SOCKET_FD, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+        {
+            printf("Failed to connect\n");
+            exit(0);
+        }
+    }
+    else
+    {
+        SOCKET_FD = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (SOCKET_FD == -1)
+        {
+            printf("Failed to open socket\n");
+            exit(0);
+        }
+
+        struct sockaddr_un addr;
+        addr.sun_family = AF_UNIX;
+        strcpy(addr.sun_path, UNIX_PATH);
+
+        if (connect(SOCKET_FD, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+        {
+            printf("Failed to connect\n");
+            exit(0);
+        }
+    }
+
+    EPOLL_FD = epoll_create1(0);
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLPRI;
+    event.data.fd = SOCKET_FD;
+
+    event_data = calloc(1, sizeof(Event_Data));
+    event_data->event_type = SOCKET;
+    event_data->socket_fd = SOCKET_FD;
+    event.data.ptr = event_data;
+
+    epoll_ctl(EPOLL_FD, EPOLL_CTL_ADD, SOCKET_FD, &event);
+}
+
+void *read_messages()
+{
+    struct epoll_event events[10];
+
+    while (running)
+    {
+        int read_amount = epoll_wait(EPOLL_FD, events, 10, -1);
+
+        for (int i = 0; i < read_amount; ++i)
+        {
+            Event_Data *event_data = (Event_Data *)events[i].data.ptr;
+
+            if (event_data->event_type == SOCKET)
+            {
+                char msg[MAX_MSG] = "";
+                recv(event_data->socket_fd, (char *)msg, MAX_MSG, 0);
+
+                if (strcmp(msg, SERVER_STOPPED) == 0)
+                {
+                    handle_stop();
+                }
+
+                if (strcmp(msg, CLIENT_REMOVED) == 0)
+                {
+                    force_stop();
+                }
+
+                if (strcmp(msg, CLIENT_REMOVED) == 0)
+                {
+                    force_stop();
+                }
+
+                if (strcmp(msg, PING) == 0 || strcmp(msg, CONNECTED) == 0)
+                {
+                    send(SOCKET_FD, PING, strlen(PING), 0);
+                    continue;
+                }
+
+                if (strlen(msg) > 0)
+                {
+                    printf("%s\n", msg);
+                }
+            }
+        }
+    }
+
+    free(event_data);
+    shutdown(SOCKET_FD, SHUT_RDWR);
+    close(SOCKET_FD);
+
+    return NULL;
+}
+
+void handle_stop()
+{
+    running = false;
+    send(SOCKET_FD, STOP, strlen(STOP), 0);
+    pthread_join(read_thread, NULL);
+
+    exit(0);
+}
+
+void force_stop()
+{
+    running = false;
+    pthread_join(read_thread, NULL);
+    exit(0);
+}
---
+++ lab10/BertMichal/cw10/zad1/server.c
@@ -0,0 +1,467 @@
+#include "common.h"
+
+#define MAX_CLIENTS 10
+
+typedef struct Client
+{
+    char *nick;
+    int socket_id;
+    Event_Data *event_data;
+    bool active;
+} Client;
+
+char *UNIX_PATH;
+int PORT, WEB_SOCKET_FD, UNIX_SOCKET_FD, EPOLL_FD;
+Client *clients[MAX_CLIENTS];
+Event_Data *web_event_data, *unix_event_data;
+bool running = true;
+
+pthread_t connection_thread, ping_thread;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+FILE *log_file;
+
+int find_client_index();
+
+void *connections_handler();
+void *ping();
+
+void refresh_user(int client_fd);
+
+void handle_stop();
+
+void init_web_socket();
+void init_unix_socket();
+
+bool add_client(char *nick, int client_fd);
+void remove_client(int client_fd);
+void list_users(int client_fd);
+void send_to_one(char *nick, char *message);
+void send_to_all(char *message);
+
+void perform_log(int client_fd, char *message);
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Too few arguments!\n");
+        return -1;
+    }
+
+    PORT = atoi(argv[1]);
+    UNIX_PATH = argv[2];
+
+    EPOLL_FD = epoll_create1(0);
+
+    init_web_socket();
+    init_unix_socket();
+
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        clients[i] = calloc(1, sizeof(Client));
+        // clients[i]->nick = calloc(MAX_NICK, sizeof(char));
+        clients[i]->socket_id = -1;
+        clients[i]->active = false;
+        clients[i]->event_data = NULL;
+    }
+
+    pthread_create(&connection_thread, NULL, connections_handler, NULL);
+    pthread_create(&ping_thread, NULL, ping, NULL);
+
+    struct sigaction action;
+    action.sa_handler = &handle_stop;
+    sigaction(SIGINT, &action, NULL);
+
+    log_file = fopen("log.txt", "w");
+
+    pthread_join(connection_thread, NULL);
+    pthread_join(ping_thread, NULL);
+
+    return 0;
+}
+
+int find_client_index()
+{
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        if (clients[i]->socket_id == -1)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+bool add_client(char *nick, int client_fd)
+{
+    int new_client_id;
+    if ((new_client_id = find_client_index()) < 0)
+    {
+        return false;
+    }
+
+    // clients[new_client_id]->nick = nick;
+    clients[new_client_id]->nick = calloc(MAX_NICK, sizeof(char));
+    strcpy(clients[new_client_id]->nick, nick);
+    clients[new_client_id]->socket_id = client_fd;
+    clients[new_client_id]->active = true;
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLPRI;
+    event.data.fd = client_fd;
+
+    clients[new_client_id]->event_data = calloc(1, sizeof(Event_Data));
+    clients[new_client_id]->event_data->event_type = CLIENT;
+    clients[new_client_id]->event_data->socket_fd = client_fd;
+    event.data.ptr = clients[new_client_id]->event_data;
+
+    epoll_ctl(EPOLL_FD, EPOLL_CTL_ADD, client_fd, &event);
+
+    return true;
+}
+
+void remove_client(int client_fd)
+{
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        if (clients[i]->socket_id == client_fd)
+        {
+            free(clients[i]->nick);
+            clients[i]->socket_id = -1;
+            clients[i]->active = false;
+            free(clients[i]->event_data);
+
+            shutdown(client_fd, SHUT_RDWR);
+            close(client_fd);
+            return;
+        }
+    }
+    return;
+}
+
+void list_users(int client_fd)
+{
+    char active_users[512] = "";
+
+    printf("LISTING\n");
+
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        if (clients[i]->socket_id != -1)
+        {
+            strcat(active_users, clients[i]->nick);
+            strcat(active_users, " ");
+        }
+    }
+
+    printf("%s\n", active_users);
+
+    send(client_fd, active_users, strlen(active_users), 0);
+}
+
+void send_to_one(char *nick, char *message)
+{
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        if (clients[i]->socket_id != -1 && strcmp(nick, clients[i]->nick) == 0)
+        {
+            send(clients[i]->socket_id, message, strlen(message), 0);
+        }
+    }
+}
+
+void send_to_all(char *message)
+{
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        send(clients[i]->socket_id, message, strlen(message), 0);
+    }
+}
+
+void init_web_socket()
+{
+    WEB_SOCKET_FD = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (WEB_SOCKET_FD == -1)
+    {
+        printf("Couldn't open socket\n");
+        exit(0);
+    }
+
+    struct sockaddr_in web_address;
+    web_address.sin_family = AF_INET;
+    web_address.sin_port = htons(PORT);
+    web_address.sin_addr.s_addr = INADDR_ANY;
+
+    int reuse = 1;
+    if (setsockopt(WEB_SOCKET_FD, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1)
+    {
+        printf("Couldn't set reuse option\n");
+        exit(0);
+    }
+
+    if (bind(WEB_SOCKET_FD, (struct sockaddr *)&web_address, sizeof(web_address)) == -1)
+    {
+        printf("Couldn't bind socket");
+        exit(0);
+    }
+
+    if (listen(WEB_SOCKET_FD, MAX_CLIENTS) == -1)
+    {
+        printf("Failed to listen\n");
+        exit(0);
+    }
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLPRI;
+    event.data.fd = WEB_SOCKET_FD;
+
+    web_event_data = calloc(1, sizeof(Event_Data));
+    web_event_data->event_type = SOCKET;
+    web_event_data->socket_fd = WEB_SOCKET_FD;
+    event.data.ptr = web_event_data;
+
+    epoll_ctl(EPOLL_FD, EPOLL_CTL_ADD, WEB_SOCKET_FD, &event);
+}
+
+void init_unix_socket()
+{
+    UNIX_SOCKET_FD = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (UNIX_SOCKET_FD == -1)
+    {
+        printf("Couldn't open socket\n");
+        exit(0);
+    }
+
+    struct sockaddr_un unix_address;
+    unix_address.sun_family = AF_UNIX;
+    strcpy(unix_address.sun_path, UNIX_PATH);
+
+    if (bind(UNIX_SOCKET_FD, (struct sockaddr *)&unix_address, sizeof(unix_address)) == -1)
+    {
+        printf("Couldn't bind socket");
+        exit(0);
+    }
+
+    if (listen(UNIX_SOCKET_FD, MAX_CLIENTS) == -1)
+    {
+        printf("Failed to listen\n");
+        exit(0);
+    }
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLPRI;
+    event.data.fd = UNIX_SOCKET_FD;
+
+    unix_event_data = calloc(1, sizeof(Event_Data));
+    unix_event_data->event_type = SOCKET;
+    unix_event_data->socket_fd = UNIX_SOCKET_FD;
+    event.data.ptr = unix_event_data;
+
+    epoll_ctl(EPOLL_FD, EPOLL_CTL_ADD, UNIX_SOCKET_FD, &event);
+}
+
+void handle_stop()
+{
+    running = false;
+
+    printf("\nWaiting for connection thread...\n");
+    pthread_join(connection_thread, NULL);
+    printf("Waiting for ping thread...\n");
+    pthread_join(ping_thread, NULL);
+
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (clients[i]->socket_id != -1)
+        {
+            send(clients[i]->socket_id, SERVER_STOPPED, strlen(SERVER_STOPPED), 0);
+            free(clients[i]->event_data);
+            free(clients[i]->nick);
+            close(clients[i]->socket_id);
+        }
+    }
+
+    shutdown(WEB_SOCKET_FD, SHUT_RDWR);
+    shutdown(UNIX_SOCKET_FD, SHUT_RDWR);
+
+    close(WEB_SOCKET_FD);
+    close(UNIX_SOCKET_FD);
+
+    remove(UNIX_PATH);
+
+    free(web_event_data);
+    free(unix_event_data);
+
+    fclose(log_file);
+
+    exit(0);
+}
+
+void *connections_handler()
+{
+    struct epoll_event events[MAX_CLIENTS];
+
+    while (running)
+    {
+        int read_amount = epoll_wait(EPOLL_FD, events, MAX_CLIENTS, 10);
+
+        for (int i = 0; i < read_amount; ++i)
+        {
+            Event_Data *event_data = (Event_Data *)events[i].data.ptr;
+
+            if (event_data->event_type == SOCKET)
+            {
+                int client_fd;
+
+                if ((client_fd = accept(event_data->socket_fd, NULL, NULL)) < 0)
+                {
+                    continue;
+                }
+
+                char *nick = calloc(MAX_NICK, sizeof(char));
+                recv(client_fd, (char *)nick, MAX_NICK, 0);
+
+                if (!add_client(nick, client_fd))
+                {
+                    send(client_fd, FULL, strlen(FULL), 0);
+                    close(client_fd);
+
+                    continue;
+                }
+
+                send(client_fd, CONNECTED, strlen(CONNECTED), 0);
+
+                free(nick);
+            }
+            else if (event_data->event_type == CLIENT)
+            {
+                char *msg = calloc(MAX_MSG, sizeof(char));
+                recv(event_data->socket_fd, (char *)msg, MAX_MSG, 0);
+
+                printf("GOT: [%d] %s\n", event_data->socket_fd, msg);
+
+                if (strlen(msg) == 0)
+                {
+                    continue;
+                }
+
+                perform_log(event_data->socket_fd, msg);
+
+                char *command = strtok(msg, DELIMITER);
+
+                if (strcmp(command, PING) == 0)
+                {
+                    refresh_user(event_data->socket_fd);
+                }
+
+                if (strcmp(command, LIST) == 0)
+                {
+                    list_users(event_data->socket_fd);
+                }
+
+                if (strcmp(command, STOP) == 0)
+                {
+                    pthread_mutex_lock(&mutex);
+                    remove_client(event_data->socket_fd);
+                    pthread_mutex_unlock(&mutex);
+                }
+
+                if (strcmp(command, TO_ALL) == 0)
+                {
+                    char message[MAX_MSG] = "";
+                    char *tmp;
+
+                    while ((tmp = strtok(NULL, DELIMITER)) != NULL)
+                    {
+                        strcat(message, tmp);
+                        strcat(message, " ");
+                    }
+
+                    send_to_all(message);
+                }
+
+                if (strcmp(command, TO_ONE) == 0)
+                {
+                    char *other_nick = strtok(NULL, DELIMITER);
+
+                    char message[MAX_MSG] = "";
+                    char *tmp;
+
+                    while ((tmp = strtok(NULL, DELIMITER)) != NULL)
+                    {
+                        strcat(message, tmp);
+                        strcat(message, " ");
+                    }
+
+                    send_to_one(other_nick, message);
+                }
+
+                free(msg);
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void refresh_user(int client_fd)
+{
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+    {
+        if (clients[i]->socket_id == client_fd)
+        {
+            clients[i]->active = true;
+            return;
+        }
+    }
+}
+
+void *ping()
+{
+    while (running)
+    {
+        for (int i = 0; i < MAX_CLIENTS; ++i)
+        {
+            if (clients[i]->socket_id == -1)
+            {
+                continue;
+            }
+
+            clients[i]->active = false;
+
+            send(clients[i]->socket_id, PING, strlen(PING), 0);
+
+            sleep(PING_TIMEOUT);
+
+            if (!clients[i]->active && clients[i]->socket_id != -1)
+            {
+                send(clients[i]->socket_id, CLIENT_REMOVED, strlen(CLIENT_REMOVED), 0);
+
+                pthread_mutex_lock(&mutex);
+                remove_client(clients[i]->socket_id);
+                pthread_mutex_unlock(&mutex);
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void perform_log(int client_fd, char *message)
+{
+    char log[2 * MAX_MSG];
+    char date[64];
+
+    time_t rawtime;
+    time(&rawtime);
+
+    struct tm *timeinfo = localtime(&rawtime);
+
+    sprintf(date, "%s", asctime(timeinfo));
+    date[strlen(date) - 1] = '\0'; // Remove \n
+
+    sprintf(log, "[%s] [FD: %d] %s\n", date, client_fd, message);
+    fwrite(log, sizeof(char), strlen(log), log_file);
+}
